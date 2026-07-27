8ab28076e1b16f459b9e22098dead27bdee8e843
opsbyNikhil <purposejob97@gmail.com>
2026-07-28 00:22:25 +0530

    urls

--- ecom_app/views.py
+++ ecom_app/views.py
@@ -379,7 +379,7 @@ def place_order(request):
 @login_required(login_url='login')
 def my_orders(request):
     orders = Order.objects.filter(user=request.user).order_by('-created_at')
-    return render(request, 'my_orders.html', {'orders': orders})
+    return render(request, 'cart/my_orders.html', {'orders': orders})
 
 @login_required(login_url='login')
 def order_success(request, order_id):
